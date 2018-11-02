00436e6d3f715acb4819e88d4c8b2e817054344a
Brian Jennings <Briant717@gmail.com>
2018-11-02 13:26:22 -0400

    Added seven segment code

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,13 +1,17 @@
 
+from Adafruit_LED_Backpack import SevenSegment
+
 # Setup
 ampm = 0;
 alarm = 0;
 alarm_hour = 0;
 alarm_minute = 0;
+segment = SevenSegment.SevenSegment(address=0x70)
+segment.begin()
 
 update()
 
-def update:
+def update():
     while(1):
         datetime_hour = datetime.datetime.now().hour
         minute = datetime.datetime.now().minute
@@ -59,5 +63,20 @@ def show_alarm():
     set_7seg(a_hour, alarm_minute)
 
 def set_7seg(hour, minute):
-    # code for setting seven segment display
+    segment.clear()
+    # Set hours
+    segment.set_digit(0, int(hour / 10))     # Tens
+    segment.set_digit(1, hour % 10)          # Ones
+    # Set minutes
+    segment.set_digit(2, int(minute / 10))   # Tens
+    segment.set_digit(3, minute % 10)        # Ones
+    # Toggle colon
+    segment.set_colon(second % 2)              # Toggle colon at 1Hz
+
+    # Write the display buffer to the hardware.  This must be called to
+    # update the actual display LEDs.
+    segment.write_display()
+
+    # Wait a quarter second (less than 1 second to prevent colon blinking getting$
+    time.sleep(0.25)
     return 0
