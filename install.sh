d0effa85753e48c5fc7ea7846e4d1c8532cb7a48
Brian Jennings <Briant717@gmail.com>
2018-11-13 18:59:18 -0500

    added install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+config-pin P9_19 i2c
+config-pin P9_20 i2c
+config-pin P9_21 gpio
+config-pin P9_22 gpio
+config-pin P9_23 gpio
+config-pin P9_24 gpio
+config-pin P9_25 gpio
+config-pin P9_26 gpio
+config-pin P9_27 gpio
+sudo apt-get install vlc
+sudo apt install python2.7
+sudo apt-get install python-pip
+sudo pip install Adafruit_LED_Backpack
+apt-get install pulseaudio pulseaudio-module-bluetooth pavucontrol bluez-firmware
